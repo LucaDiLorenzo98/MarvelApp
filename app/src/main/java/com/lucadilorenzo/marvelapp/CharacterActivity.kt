a056a7b02eefd963b63218a68c2274b20da814c9
luke9598 <57172969+luke9598@users.noreply.github.com>
2021-06-20 16:30:59 +0200

    dio cane

--- app/src/main/java/com/lucadilorenzo/marvelapp/CharacterActivity.kt
+++ app/src/main/java/com/lucadilorenzo/marvelapp/CharacterActivity.kt
@@ -1,8 +1,6 @@
 package com.lucadilorenzo.marvelapp
 
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.lucadilorenzo.marvelapp.databinding.ActivityCharacterBinding
